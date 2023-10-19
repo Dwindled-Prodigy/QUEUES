d3cd9e3add15b55a491666206052bb0968cf80a9
Darshan Kumar T <darshan_kumar_t@outlook.com>
2023-10-19 10:28:04 +0530

    Create Find.cpp

---
+++ Find.cpp
@@ -0,0 +1,11 @@
+int find(int item) {
+    if (isEmpty()) {
+        return -1;  // Queue is empty; item cannot be found.
+    }
+    for (int i = front, position = 0; i <= rear; i++, position++) {
+        if (array[i] == item) {
+            return position;  // Item found at position 'position'.
+        }
+    }
+    return -1;  // Item not found in the queue.
+}
