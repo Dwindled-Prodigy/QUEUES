239e963ad41ce18ad1cc0f8492169826a80ccb69
Darshan Kumar T <darshan_kumar_t@outlook.com>
2023-10-19 10:20:55 +0530

    Create Search.java

---
+++ Search.java
@@ -0,0 +1,11 @@
+public boolean search(int item) {
+    if (isEmpty()) {
+        return false;  // Queue is empty; item cannot be found.
+    }
+    for (int i = front; i <= rear; i++) {
+        if (array.get(i) == item) {
+            return true;  // Item found in the queue.
+        }
+    }
+    return false;  // Item not found in the queue.
+}
